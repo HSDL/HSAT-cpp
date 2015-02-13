faad53a6000982a1d716ecffb61ec1e0f6e7f7b3
cmccomb <ccmcc2012@gmail.com>
2015-02-13 09:28:04 -0500

    Added limiting v. non-limiting condition to quadratic stochastic univariate search.

--- src/metaoptim.cpp
+++ src/metaoptim.cpp
@@ -131,35 +131,50 @@ UnivariateSearch::UnivariateSearch(string file_name){
 }
 
 void UnivariateSearch::solve(int max_iter){
-    // Define a few initial things
+    // Stores the current iteration and values
     current_iteration = 0;
+    Parameters p_current;
+    // Stores a new value
     double new_val = 0;
+    // Keeps track of whether or not an edge solution has been found
     bool EDGE_SOLUTION;
-    vector<double> quad_res;
+    // Keeps track of whether a bound is being approached
+    bool APPROACHING_BOUND;
+    // Stores values to feed to quadratic regression
     vector<double> X;
     vector<double> Y;
+    // Stores the results from the quadratic regression
+    vector<double> quad_res;
     double temp_ub;
     double temp_lb;
-    Parameters p_current;
     cout << "\nBeginning Optimization Routine" << endl;
 
     // Do some iterations, bro. Get mad optimized.
     while(current_iteration < max_iter) {
         cout << "\tIteration " << current_iteration << endl;
         EDGE_SOLUTION = false;
+        APPROACHING_BOUND = false;
         // Within each iteration, step in each direction
         for (int i = 0; i < var_name.size(); i++) {
             cout << "\t\tComputing " << var_name[i] ;
 
+            // Draw a random variable in the param range
+            temp_ub = min(var_vals[i]+step_sizes[i], upper_lims[i]);
+            temp_lb = max(var_vals[i]-step_sizes[i], lower_lims[i]);
+
+            // Check to see if we're up against a bound
+            if(temp_ub == upper_lims[i] || temp_lb == lower_lims[i]){
+                APPROACHING_BOUND = true;
+            }
+
             // Initialize vectors for regression
             for (int j = 0; j < p_best.n_reps; j++){
-                // Draw a random variable in the param range
-                temp_ub = min(var_vals[i]+step_sizes[i], upper_lims[i]);
-                temp_lb = max(var_vals[i]-step_sizes[i], lower_lims[i]);
+
+                // Define a new value
                 new_val = uniform(temp_ub, temp_lb);
                 X.push_back(new_val);
 
-                // Push that variable into the
+                // Create a parameter set with the new value
                 p_current = p_best;
                 p_current.set_from_pair(var_name[i], new_val);
 
@@ -177,8 +192,12 @@ void UnivariateSearch::solve(int max_iter){
                 p_best.set_from_pair(var_name[i], quad_res[0]);
                 var_vals[i] = quad_res[0];
             }
+
+            // Check to see if we're against a non-limiting edge solution
             if (quad_res[0] == vector_min(X) || quad_res[0] == vector_max(X)){
-                EDGE_SOLUTION = true;
+                if (!APPROACHING_BOUND) {
+                    EDGE_SOLUTION = true;
+                }
             }
 
             cout << "\t= " << quad_res[0]
