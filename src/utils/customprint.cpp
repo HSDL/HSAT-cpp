dc20e555f12e4a5f909bd2de227efa030f9b3e56
cmccomb <ccmcc2012@gmail.com>
2015-02-24 10:30:42 -0500

    General cleanup.

--- src/utils/customprint.cpp
+++ src/utils/customprint.cpp
@@ -34,43 +34,43 @@ void print(vector<double> x){
 //// Save a vector to a file
 void save_to_file(string file_path, vector<double> x){
     // Open a file stream
-    ofstream myfile;
+    ofstream my_file;
 
     // Open a file on that file stream
-    myfile.open(file_path);
+    my_file.open(file_path);
 
     // Set scientific notation. Mostly for nice alignment
-    myfile << scientific;
+    my_file << scientific;
 
     // Step through the vector, and print an element on each line
     for(int i=0; i<x.size(); i++){
-        myfile << x[i] << endl;
+        my_file << x[i] << endl;
     }
 
     // Close the file
-    myfile.close();
+    my_file.close();
 }
 
 
 //// Save a vector of vectors to file. This happens a lot, actually.
 void save_to_file(string file_path, vector<vector<double>> x){
     // Open a file stream
-    ofstream myfile;
+    ofstream my_file;
 
     // Open a file on that file stream
-    myfile.open(file_path);
+    my_file.open(file_path);
 
     // Set to scientific notation, mostly for alignment
-    myfile << scientific;
+    my_file << scientific;
 
     // Print each sub-vector to a column.
     for(int i=0; i<x[0].size(); i++) {
         for(int j=0; j<x.size(); j++){
-            myfile << x[j][i] << "\t";
+            my_file << x[j][i] << "\t";
         }
-        myfile << endl;
+        my_file << endl;
     }
 
     // Close the file
-    myfile.close();
+    my_file.close();
 }
