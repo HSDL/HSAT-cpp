a72ffac135fc0800bff46649a5d158513959faa9
cmccomb <ccmcc2012@gmail.com>
2015-03-02 08:48:27 -0500

    Made a general main function.

--- test/test_bench.sh
+++ test/test_bench.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Test the benchmarking functionality
+cd ..
 ./build/bin/hsat --bench --input ./test/example_bench.txt --output ./test/output_bench.txt --verbose
 
 exit 0
--- test/test_metaoptim.sh
+++ test/test_metaoptim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Test the metaoptimization functionality
+cd ..
 ./build/bin/hsat --univariate 5 --input ./test/example_bench.txt --output ./test/output_bench.txt --verbose
 
 exit 0
