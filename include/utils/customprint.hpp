40d96e6592d5cf2739598d0013f4132496585e7c
cmccomb <ccmcc2012@gmail.com>
2015-03-25 12:11:40 -0400

    No more long doubles.

--- include/utils/customprint.hpp
+++ include/utils/customprint.hpp
@@ -11,15 +11,11 @@ using namespace std;
 // Functions to print variables
 void print(int x);
 void print(double x);
-void print(double x);
-void print(vector<double>);
 void print(vector<double>);
 void print(vector<double>::size_type x);
 
 // Write to file
 void save_to_file(string file_path, vector<double>);
-void save_to_file(string file_path, vector<double>);
-void save_to_file(string file_path, vector< vector<double> >);
 void save_to_file(string file_path, vector< vector<double> >);
 
 #endif
