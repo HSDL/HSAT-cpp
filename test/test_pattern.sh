451490b4911c48f9721855fdd6dfef4316761380
cmccomb <ccmcc2012@gmail.com>
2015-03-26 16:48:46 -0400

    Fixed test.

--- test/test_pattern.sh
+++ test/test_pattern.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Test the metaoptimization functionality
-../build/bin/hsat --pattern 5 --input example_bench.txt --output output_bench.txt --verbose
+../build/bin/hsat --pattern 5 --input example_metaoptim.txt --output output_metaoptim.txt --verbose
 
 exit 0
