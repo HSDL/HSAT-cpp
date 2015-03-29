35075a36c299e9c7c59f0126ac6c09f1a300a537
Chris McComb <ccmcc2012@gmail.com>
2015-03-29 19:54:26 -0400

    Update hsat.cpp

--- src/hsat.cpp
+++ src/hsat.cpp
@@ -5,9 +5,10 @@
 #include <sys/time.h>
 #include <omp.h>
 
+int main(int argc, char *argv[]) {
+
 omp_set_num_threads(4);
 
-int main(int argc, char *argv[]) {
     // Random seed
     seed_time();
 
